4da391f06980bc07e6db881c640e45e574d31259
Ruben Steen <rubensteen123@gmail.com>
2023-04-21 14:05:13 +0000

    Added password pests

--- tests/Feature/Jetstream/UpdatePasswordTest.php
+++ tests/Feature/Jetstream/UpdatePasswordTest.php
@@ -4,41 +4,41 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 test('password can be updated', function () {
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs($user = User::factory()->create(['password' => Hash::make('Password123!')]));
 
     $response = $this->put('/user/password', [
-        'current_password' => 'password',
-        'password' => 'new-password',
-        'password_confirmation' => 'new-password',
+        'current_password' => 'Password123!',
+        'password' => 'new-Password123!',
+        'password_confirmation' => 'new-Password123!',
     ]);
 
-    expect(Hash::check('new-password', $user->fresh()->password))->toBeTrue();
+    expect(Hash::check('new-Password123!', $user->fresh()->password))->toBeTrue();
 });
 
 test('current password must be correct', function () {
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs($user = User::factory()->create(['password' => Hash::make('Password123!')]));
 
     $response = $this->put('/user/password', [
-        'current_password' => 'wrong-password',
-        'password' => 'new-password',
-        'password_confirmation' => 'new-password',
+        'current_password' => 'wrong-Password123!',
+        'password' => 'new-Password123!',
+        'password_confirmation' => 'new-Password123!',
     ]);
 
     $response->assertSessionHasErrors();
 
-    expect(Hash::check('password', $user->fresh()->password))->toBeTrue();
+    expect(Hash::check('Password123!', $user->fresh()->password))->toBeTrue();
 });
 
 test('new passwords must match', function () {
-    $this->actingAs($user = User::factory()->create());
+    $this->actingAs($user = User::factory()->create(['password' => Hash::make('Password123!')]));
 
     $response = $this->put('/user/password', [
-        'current_password' => 'password',
+        'current_password' => 'Password123!',
         'password' => 'new-password',
         'password_confirmation' => 'wrong-password',
     ]);
 
     $response->assertSessionHasErrors();
 
-    expect(Hash::check('password', $user->fresh()->password))->toBeTrue();
+    expect(Hash::check('Password123!', $user->fresh()->password))->toBeTrue();
 });
