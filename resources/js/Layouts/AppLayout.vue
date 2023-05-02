9ee73be335a1163fd6815217609e8a518dd20731
Ruben Steen <rubensteen123@gmail.com>
2023-05-02 14:23:35 +0000

    Changed some names

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -22,12 +22,12 @@ import {
 import { Bars3Icon, ChevronRightIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
 
 const navigation = [
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Deployments', href: '#', icon: ServerIcon, current: true },
-  { name: 'Activity', href: '#', icon: SignalIcon, current: false },
-  { name: 'Domains', href: '#', icon: GlobeAltIcon, current: false },
-  { name: 'Usages', href: '#', icon: ChartBarSquareIcon, current: false },
-  { name: 'Settings', href: '#', icon: Cog6ToothIcon, current: false },
+  { name: 'Dashboard', href: '#', icon: FolderIcon, current: false },
+  { name: 'Potjes', href: '#', icon: ServerIcon, current: true },
+  { name: 'Inkomen', href: '#', icon: SignalIcon, current: false },
+  { name: 'Uitgaven', href: '#', icon: GlobeAltIcon, current: false },
+  { name: 'Sparen', href: '#', icon: ChartBarSquareIcon, current: false },
+  { name: 'Regels', href: '#', icon: Cog6ToothIcon, current: false },
 ]
 const teams = [
   { id: 1, name: 'Planetaria', href: '#', initial: 'P', current: false },
