aa1cf7189be18aa31e4e31a104a253c50ad9b483
Ruben Steen <rubensteen123@gmail.com>
2023-05-03 18:39:25 +0000

    Frontend

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -24,6 +24,12 @@ import {
   FolderIcon,
   HomeIcon,
   UsersIcon,
+  WalletIcon,
+  HomeModernIcon,
+  BanknotesIcon,
+  CreditCardIcon,
+  CurrencyEuroIcon,
+  RectangleStackIcon,
 } from '@heroicons/vue/24/outline'
 
 defineProps({
@@ -32,11 +38,14 @@ defineProps({
 
 const navigation = [
   { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
-  { name: 'Team', href: '#', icon: UsersIcon, current: false },
-  { name: 'Projects', href: '#', icon: FolderIcon, current: false },
-  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
-  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
-  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+  { name: 'Potjes', href: '#', icon: RectangleStackIcon, current: false },
+]
+
+const secondNavigation = [
+  { name: 'Uitgaven', href: '#', icon: BanknotesIcon, current: false },
+  { name: 'Gezamelijke Uitgaven', href: '#', icon: CreditCardIcon, current: false },
+  { name: 'Sparen', href: '#', icon: WalletIcon, current: false },
+  { name: 'Gezamelijk Sparen', href: '#', icon: HomeModernIcon, current: false },
 ]
 
 const userNavigation = [
@@ -50,6 +59,49 @@ const stats = [
   { id: 2, name: 'Dagen te gaan', stat: '14',},
 ]
 
+const monthStats = [
+  {
+    name: 'Uitgaven',
+    amount: '€249.27',
+    icon: BanknotesIcon,
+  },
+  {
+    name: 'Gezamelijke Uitgaven',
+    amount: '€1,334.37',
+    icon: CreditCardIcon,
+  },
+  {
+    name: 'Sparen',
+    amount: '€130.00',
+    icon: WalletIcon,
+  },
+  {
+    name: 'Gezamelijk Sparen',
+    amount: '€375.00',
+    icon: HomeModernIcon,
+  },
+  // More people...
+]
+
+const transactions = [
+  {
+    id: 1,
+    name: 'Boodschappen',
+    amount: '€200.00',
+    period: 'Maandelijks',
+    from: 'Mij',
+    to: 'Boodchappen',
+  },
+  {
+    id: 2,
+    name: 'Internet en Televisie',
+    amount: '€25,57',
+    period: 'Maandelijks',
+    from: 'Mij',
+    to: 'Gezamelijke rekening',
+  },
+]
+
 const sidebarOpen = ref(false)
 </script>
 
@@ -60,7 +112,7 @@ const sidebarOpen = ref(false)
   <nav>
     <div class="mx-auto px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
           <!-- Mobile menu button -->
           <button @click="sidebarOpen = true" class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
             <span class="sr-only">Open main menu</span>
@@ -68,17 +120,13 @@ const sidebarOpen = ref(false)
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
           <div class="flex flex-shrink-0 items-center">
             <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
             <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
           </div>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button" class="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            <span class="sr-only">View notifications</span>
-          </button>
-
           <!-- Profile dropdown -->
           <Menu as="div" class="relative ml-3">
             <div>
@@ -141,6 +189,16 @@ const sidebarOpen = ref(false)
                         </li>
                       </ul>
                     </li>
+                    <li>
+                      <ul role="list" class="-mx-2 space-y-1">
+                        <li v-for="item in secondNavigation" :key="item.name">
+                          <a :href="item.href" :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                            <component :is="item.icon" :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                            {{ item.name }}
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
                     <li class="mt-auto">
                       <a href="#" class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600">
                         <Cog6ToothIcon class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
@@ -168,6 +226,16 @@ const sidebarOpen = ref(false)
             <li>
               <ul role="list" class="-mx-2 space-y-1">
                 <li v-for="item in navigation" :key="item.name">
+                  <a :href="item.href" :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                    <component :is="item.icon" :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+                    {{ item.name }}
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <ul role="list" class="-mx-2 space-y-1">
+                <li v-for="item in secondNavigation" :key="item.name">
                   <a :href="item.href" :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
                     <component :is="item.icon" :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']" aria-hidden="true" />
                     {{ item.name }}
@@ -220,28 +288,86 @@ const sidebarOpen = ref(false)
 
           <div class="overflow-hidden bg-white shadow mt-4">
             <div class="px-4 py-5 sm:p-6">
-              
-              <div>
-                <h3 class="text-base font-semibold leading-6 text-gray-900">Maart 2023</h3>
 
-                <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-                  <div v-for="item in stats" :key="item.id" class="relative overflow-hidden rounded bg-indigo-50 bg-opacity-75 px-4 pb-12 pt-5 shadow sm:px-6 sm:pt-6">
-                    <dt>
-                      <p class="truncate text-sm font-medium text-gray-500">{{ item.name }}</p>
-                    </dt>
-                    <dd class="flex items-baseline">
-                      <p class="text-2xl font-semibold text-gray-900">{{ item.stat }}</p>
-                      <div class="absolute inset-x-0 bottom-0 px-4 py-3 sm:px-6">
-                        <div class="text-sm">
-                          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"
-                            >View all<span class="sr-only"> {{ item.name }} stats</span></a
-                          >
-                        </div>
-                      </div>
-                    </dd>
+              <div class="grid grid-cols-1 gap-4 pb-4 lg:w-2/4 mx-auto">
+                <div class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
+                  <div class="flex-shrink-0">
+                    <component :is="CurrencyEuroIcon" class="h-10 w-10 shrink-0" aria-hidden="true" />
                   </div>
-                </dl>
+                  <div class="min-w-0 flex-1">
+                    <a href="#" class="focus:outline-none">
+                      <span class="absolute inset-0" aria-hidden="true" />
+                      <p class="text-sm font-medium text-gray-900">Inkomen</p>
+                      <p class="truncate text-sm text-gray-500">€3857,45</p>
+                    </a>
+                  </div>
+                </div>
               </div>
+              
+              <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                <div v-for="stat in monthStats" :key="stat.name" class="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400">
+                  <div class="flex-shrink-0">
+                    <component :is="stat.icon" class="h-10 w-10 shrink-0" aria-hidden="true" />
+                  </div>
+                  <div class="min-w-0 flex-1">
+                    <a href="#" class="focus:outline-none">
+                      <span class="absolute inset-0" aria-hidden="true" />
+                      <p class="text-sm font-medium text-gray-900">{{ stat.name }}</p>
+                      <p class="truncate text-sm text-gray-500">{{ stat.amount }}</p>
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="overflow-hidden bg-white shadow mt-4">
+            <div class="px-4 py-5 sm:p-6">
+
+                <div class="sm:flex sm:items-center">
+                  <div class="sm:flex-auto">
+                    <h1 class="text-base font-semibold leading-6 text-gray-900">Transactions</h1>
+                    <p class="mt-2 text-sm text-gray-700">A table of placeholder stock market data that does not make any sense.</p>
+                  </div>
+                  <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                    <button type="button" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Export</button>
+                  </div>
+                </div>
+                <div class="mt-8 flow-root">
+                  <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                      <table class="min-w-full divide-y divide-gray-300">
+                        <thead>
+                          <tr>
+                            <th scope="col" class="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Naam</th>
+                            <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Kosten gemiddeld</th>
+                            <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Herhaling</th>
+                            <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Van</th>
+                            <th scope="col" class="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900">Naar</th>
+                            <th scope="col" class="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0">
+                              <span class="sr-only">Edit</span>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200 bg-white">
+                          <tr v-for="transaction in transactions" :key="transaction.id">
+                            <td class="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">{{ transaction.name }}</td>
+                            <td class="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">{{ transaction.amount }}</td>
+                            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-900">{{ transaction.period }}</td>
+                            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ transaction.from }}</td>
+                            <td class="whitespace-nowrap px-2 py-2 text-sm text-gray-500">{{ transaction.to }}</td>
+                            <td class="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                              <a href="#" class="text-indigo-600 hover:text-indigo-900"
+                                >Edit<span class="sr-only">, {{ transaction.id }}</span></a
+                              >
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
 
             </div>
           </div>
