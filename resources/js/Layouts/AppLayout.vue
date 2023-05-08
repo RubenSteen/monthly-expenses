b4cb09fdd72f34695fcc1b9b2efe5922ed3b2275
Ruben Steen <rubensteen123@gmail.com>
2023-05-08 09:31:23 +0000

    Added profile photo

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -79,9 +79,8 @@ const sidebarOpen = ref(false)
               <MenuButton
                 class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 <span class="sr-only">Open user menu</span>
-                <img class="h-8 w-8 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt="" />
+                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url"
+                  :alt="$page.props.auth.user.name" />
               </MenuButton>
             </div>
             <transition enter-active-class="transition ease-out duration-200"
