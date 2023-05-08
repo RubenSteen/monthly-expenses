56bcd196dfb37cbafb8ceacbdd6d2b2cad7943e9
Ruben Steen <rubensteen123@gmail.com>
2023-05-08 09:34:53 +0000

    Added admins and super admins

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->boolean('admin')->default(0);
+            $table->boolean('super_admin')->default(0);
             $table->timestamps();
         });
     }
