e161104bd77469548319c17f9615a8be724f0037
Ruben Steen <rubensteen123@gmail.com>
2023-05-08 10:30:12 +0000

    Logo clickable

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -65,12 +65,12 @@ const sidebarOpen = ref(false)
           </button>
         </div>
         <div class="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
-          <div class="flex flex-shrink-0 items-center">
+          <a :href="route('landing')" class="flex flex-shrink-0 items-center">
             <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company" />
             <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company" />
-          </div>
+          </a>
         </div>
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <!-- Profile dropdown -->
