e73a11b4ce78fc3b30cb48cfb4fbf73492c700c9
Ruben Steen <rubensteen123@gmail.com>
2023-05-09 11:51:09 +0000

    Moved form

--- resources/js/Components/CreatePiggyBankModal.vue
+++ resources/js/Components/CreatePiggyBankModal.vue
@@ -78,28 +78,29 @@ onUpdated(() => {
 
 <template>
     <Modal :show="props.show" :max-width="props.maxWidth" :closeable="props.closeable" @close="close">
-        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <form @submit.prevent="submit">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 
-            <div class="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
-                <div class="flex items-center">
-                    <div
-                        class="mx-auto shrink-0 items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10 hidden sm:inline-flex">
-                        <component :is="ClipboardDocumentListIcon" class="h-6 w-6 text-indigo-600" aria-hidden="true" />
+                <div class="border-b border-gray-200 pb-5 sm:flex sm:items-center sm:justify-between">
+                    <div class="flex items-center">
+                        <div
+                            class="mx-auto shrink-0 items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10 hidden sm:inline-flex">
+                            <component :is="ClipboardDocumentListIcon" class="h-6 w-6 text-indigo-600" aria-hidden="true" />
+                        </div>
+                        <h3 class="text-base font-semibold leading-6 text-gray-90 pl-2">
+                            <slot name="title" />
+                        </h3>
                     </div>
-                    <h3 class="text-base font-semibold leading-6 text-gray-90 pl-2">
-                        <slot name="title" />
-                    </h3>
+                    <div class="mt-3 sm:ml-4 sm:mt-0">
+                        <button type="button" v-if="editState()" @click="deletePiggyBank"
+                            class="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
+                    </div>
                 </div>
-                <div class="mt-3 sm:ml-4 sm:mt-0">
-                    <button type="button" v-if="editState()" @click="deletePiggyBank"
-                        class="inline-flex items-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Delete</button>
-                </div>
-            </div>
 
-            <div class="sm:flex sm:items-start">
-                <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <div class="mt-4 text-sm text-gray-600">
-                        <form>
+                <div class="sm:flex sm:items-start">
+                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <div class="mt-4 text-sm text-gray-600">
+
                             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-3 sm:grid-cols-6">
 
                                 <div class="sm:col-span-4">
@@ -125,26 +126,26 @@ onUpdated(() => {
                                             :class="`${form.errors.description ? 'text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500' : 'ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600 text-gray-900'}`" />
                                     </div>
                                     <p class="mt-2 text-sm text-red-600" id="description-error"
-                                        v-if="form.errors.description">{{
+                                        v-if="form.errors.description">
+                                        {{
                                             form.errors.description
                                         }}</p>
                                 </div>
 
                             </div>
-
-
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="flex flex-row justify-end px-6 bg-gray-100 text-right">
-            <div class="my-2 flex items-center justify-end gap-x-6">
-                <button @click="close" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-                <button type="submit" @click="submit" :disabled="form.processing"
-                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <div class="flex flex-row justify-end px-6 bg-gray-100 text-right">
+                <div class="my-2 flex items-center justify-end gap-x-6">
+                    <button @click="close" type="button"
+                        class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                    <button type="submit" :disabled="form.processing"
+                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                </div>
             </div>
-        </div>
+        </form>
     </Modal>
 </template>
