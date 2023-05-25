054556b81f356e3798b5a0d1e9a2b8517a0be9eb
Ruben Steen <rubensteen123@gmail.com>
2023-05-25 14:04:39 +0000

    Added visuals for category

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -39,6 +39,13 @@ const secondNavigation = [
   { name: 'Inkomen', href: route('income.index'), icon: CurrencyEuroIcon, current: route().current('income.index') },
 ]
 
+const categoryNavigation = [
+  { name: 'Website redesign', href: '#', initial: 'W', current: false },
+  { name: 'GraphQL API', href: '#', initial: 'G', current: false },
+  { name: 'Customer migration guides', href: '#', initial: 'C', current: false },
+  { name: 'Profit sharing program', href: '#', initial: 'P', current: false },
+]
+
 const userNavigation = [
   { name: 'Profiel', href: route('profile.show'), current: route().current('profile.show') },
 ]
@@ -165,6 +172,20 @@ const sidebarOpen = ref(false)
                         </li>
                       </ul>
                     </li>
+                    <li>
+                      <div class="text-xs font-semibold leading-6 text-gray-400">Categorieën</div>
+                      <ul role="list" class="-mx-2 mt-2 space-y-1">
+                        <li v-for="item in categoryNavigation" :key="item.name">
+                          <a :href="item.href"
+                            :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-100', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                            <span
+                              :class="[item.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
+                                item.initial }}</span>
+                            <span class="truncate">{{ item.name }}</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
                     <li class="mt-auto">
                       <Link :href="route('logout')" method="post" as="button" type="button"
                         class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600 w-full">
@@ -214,6 +235,20 @@ const sidebarOpen = ref(false)
                     aria-hidden="true" />
                   {{ item.name }}
                   </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <div class="text-xs font-semibold leading-6 text-gray-400">Categorieën</div>
+              <ul role="list" class="-mx-2 mt-2 space-y-1">
+                <li v-for="item in categoryNavigation" :key="item.name">
+                  <a :href="item.href"
+                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-100', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                    <span
+                      :class="[item.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
+                        item.initial }}</span>
+                    <span class="truncate">{{ item.name }}</span>
+                  </a>
                 </li>
               </ul>
             </li>
