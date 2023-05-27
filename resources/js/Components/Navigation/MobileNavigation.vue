87aa32f6ef8aae0367994df0a4eaa7dc5a34f4fb
Ruben Steen <rubensteen123@gmail.com>
2023-05-27 08:08:04 +0000

    Nav to components

---
+++ resources/js/Components/Navigation/MobileNavigation.vue
@@ -0,0 +1,139 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import {
+    Dialog,
+    DialogPanel,
+    Menu,
+    TransitionChild,
+    TransitionRoot,
+} from '@headlessui/vue'
+import {
+    XMarkIcon,
+    HomeIcon,
+    RectangleStackIcon,
+    CurrencyEuroIcon,
+    ArrowLeftOnRectangleIcon,
+} from '@heroicons/vue/24/outline'
+
+const emit = defineEmits(['closeSidebar']);
+
+const props = defineProps({
+    sidebarOpen: {
+        type: Boolean,
+    },
+});
+
+const navigation = [
+    { name: 'Overzicht', href: route('dashboard'), icon: HomeIcon, current: route().current('dashboard') },
+    { name: 'Potjes', href: route('piggy-bank.index'), icon: RectangleStackIcon, current: route().current('piggy-bank.index') },
+]
+
+const secondNavigation = [
+    { name: 'Inkomen', href: route('income.index'), icon: CurrencyEuroIcon, current: route().current('income.index') },
+]
+
+const categoryNavigation = [
+    { name: 'Website redesign', href: '#', initial: 'W', current: false },
+    { name: 'GraphQL API', href: '#', initial: 'G', current: false },
+    { name: 'Customer migration guides', href: '#', initial: 'C', current: false },
+    { name: 'Profit sharing program', href: '#', initial: 'P', current: false },
+]
+
+const closeSidebar = () => {
+    emit('closeSidebar');
+};
+
+</script>
+
+<template>
+    <TransitionRoot as="template" :show="sidebarOpen">
+        <Dialog as="div" class="relative z-50 lg:hidden" @close="closeSidebar">
+            <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
+                enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
+                leave-to="opacity-0">
+                <div class="fixed inset-0 bg-gray-900/80" />
+            </TransitionChild>
+
+            <div class="fixed inset-0 flex">
+                <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
+                    enter-from="-translate-x-full" enter-to="translate-x-0"
+                    leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0"
+                    leave-to="-translate-x-full">
+                    <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
+                        <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
+                            enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100"
+                            leave-to="opacity-0">
+                            <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                                <button type="button" class="-m-2.5 p-2.5" @click="closeSidebar">
+                                    <span class="sr-only">Close sidebar</span>
+                                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                                </button>
+                            </div>
+                        </TransitionChild>
+                        <!-- Sidebar component, swap this element with another sidebar if you like -->
+                        <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
+                            <div class="flex h-16 shrink-0 items-center">
+                                <img class="h-8 w-auto"
+                                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                                    alt="Your Company" />
+                            </div>
+                            <nav class="flex flex-1 flex-col">
+                                <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                                    <li>
+                                        <ul role="list" class="-mx-2 space-y-1">
+                                            <li v-for="item in navigation" :key="item.name">
+                                                <Link :href="item.href"
+                                                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                                                <component :is="item.icon"
+                                                    :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
+                                                    aria-hidden="true" />
+                                                {{ item.name }}
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <ul role="list" class="-mx-2 space-y-1">
+                                            <li v-for="item in secondNavigation" :key="item.name">
+                                                <Link :href="item.href"
+                                                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                                                <component :is="item.icon"
+                                                    :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
+                                                    aria-hidden="true" />
+                                                {{ item.name }}
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <div class="text-xs font-semibold leading-6 text-gray-400">Categorieën</div>
+                                        <ul role="list" class="-mx-2 mt-2 space-y-1">
+                                            <li v-for="item in categoryNavigation" :key="item.name">
+                                                <a :href="item.href"
+                                                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-100', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
+                                                    <span
+                                                        :class="[item.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
+                                                            item.initial }}</span>
+                                                    <span class="truncate">{{ item.name }}</span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li class="mt-auto">
+                                        <Link :href="route('logout')" method="post" as="button" type="button"
+                                            class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600 w-full">
+                                        <ArrowLeftOnRectangleIcon
+                                            class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
+                                            aria-hidden="true" />
+                                        Uitloggen
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </DialogPanel>
+                </TransitionChild>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -2,25 +2,21 @@
 import { ref, computed } from 'vue'
 import { Head, Link } from '@inertiajs/vue3';
 import Notification from '@/Components/Notification.vue';
+import TopNavigation from '@/Components/Navigation/TopNavigation.vue';
+import MobileNavigation from '@/Components/Navigation/MobileNavigation.vue';
+import SideNavigation from '@/Components/Navigation/SideNavigation.vue';
 import {
   Dialog,
   DialogPanel,
   Menu,
-  MenuButton,
   MenuItem,
-  MenuItems,
   TransitionChild,
   TransitionRoot,
 } from '@headlessui/vue'
 import {
-  Bars3Icon,
   XMarkIcon,
   Cog6ToothIcon,
   HomeIcon,
-  WalletIcon,
-  HomeModernIcon,
-  BanknotesIcon,
-  CreditCardIcon,
   RectangleStackIcon,
   CurrencyEuroIcon,
   ArrowLeftOnRectangleIcon,
@@ -46,10 +42,6 @@ const categoryNavigation = [
   { name: 'Profit sharing program', href: '#', initial: 'P', current: false },
 ]
 
-const userNavigation = [
-  { name: 'Profiel', href: route('profile.show'), current: route().current('profile.show') },
-]
-
 const sidebarOpen = ref(false)
 
 </script>
@@ -59,210 +51,12 @@ const sidebarOpen = ref(false)
 
   <Notification />
 
-  <nav>
-    <div class="mx-auto px-2 sm:px-6 lg:px-8">
-      <div class="relative flex h-16 justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
-          <!-- Mobile menu button -->
-          <button @click="sidebarOpen = true"
-            class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!sidebarOpen" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
-          <a :href="route('landing')" class="flex flex-shrink-0 items-center">
-            <img class="block h-8 w-auto lg:hidden" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company" />
-            <img class="hidden h-8 w-auto lg:block" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company" />
-          </a>
-        </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <!-- Profile dropdown -->
-          <Menu as="div" class="relative ml-3">
-            <div>
-              <MenuButton
-                class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="sr-only">Open user menu</span>
-                <img class="h-10 w-10 rounded-full object-cover" :src="$page.props.auth.user.profile_photo_url"
-                  :alt="$page.props.auth.user.name" />
-              </MenuButton>
-            </div>
-            <transition enter-active-class="transition ease-out duration-200"
-              enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-              leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-              leave-to-class="transform opacity-0 scale-95">
-              <MenuItems
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <MenuItem v-slot="{ active }" v-for="link in userNavigation" :key="link.href">
-                <Link :href="link.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
-                {{
-                  link.name }} </Link>
-                </MenuItem>
-                <MenuItem v-slot="{ active }">
-                <Link :href="route('logout')" method="post" as="button" type="button"
-                  :class="[active ? 'bg-gray-100' : '', 'block w-full text-left px-4 py-2 text-sm text-gray-700']">
-                Uitloggen
-                </Link>
-                </MenuItem>
-              </MenuItems>
-            </transition>
-          </Menu>
-        </div>
-      </div>
-    </div>
-  </nav>
+  <TopNavigation :sidebarOpen="sidebarOpen" @openSidebar="sidebarOpen = true" />
 
   <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-12">
-    <TransitionRoot as="template" :show="sidebarOpen">
-      <Dialog as="div" class="relative z-50 lg:hidden" @close="sidebarOpen = false">
-        <TransitionChild as="template" enter="transition-opacity ease-linear duration-300" enter-from="opacity-0"
-          enter-to="opacity-100" leave="transition-opacity ease-linear duration-300" leave-from="opacity-100"
-          leave-to="opacity-0">
-          <div class="fixed inset-0 bg-gray-900/80" />
-        </TransitionChild>
+    <MobileNavigation :sidebarOpen="sidebarOpen" @closeSidebar="sidebarOpen = false" />
 
-        <div class="fixed inset-0 flex">
-          <TransitionChild as="template" enter="transition ease-in-out duration-300 transform"
-            enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform"
-            leave-from="translate-x-0" leave-to="-translate-x-full">
-            <DialogPanel class="relative mr-16 flex w-full max-w-xs flex-1">
-              <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0"
-                enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
-                <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                  <button type="button" class="-m-2.5 p-2.5" @click="sidebarOpen = false">
-                    <span class="sr-only">Close sidebar</span>
-                    <XMarkIcon class="h-6 w-6 text-white" aria-hidden="true" />
-                  </button>
-                </div>
-              </TransitionChild>
-              <!-- Sidebar component, swap this element with another sidebar if you like -->
-              <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-                <div class="flex h-16 shrink-0 items-center">
-                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                    alt="Your Company" />
-                </div>
-                <nav class="flex flex-1 flex-col">
-                  <ul role="list" class="flex flex-1 flex-col gap-y-7">
-                    <li>
-                      <ul role="list" class="-mx-2 space-y-1">
-                        <li v-for="item in navigation" :key="item.name">
-                          <Link :href="item.href"
-                            :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                          <component :is="item.icon"
-                            :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
-                            aria-hidden="true" />
-                          {{ item.name }}
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <ul role="list" class="-mx-2 space-y-1">
-                        <li v-for="item in secondNavigation" :key="item.name">
-                          <Link :href="item.href"
-                            :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                          <component :is="item.icon"
-                            :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
-                            aria-hidden="true" />
-                          {{ item.name }}
-                          </Link>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <div class="text-xs font-semibold leading-6 text-gray-400">Categorieën</div>
-                      <ul role="list" class="-mx-2 mt-2 space-y-1">
-                        <li v-for="item in categoryNavigation" :key="item.name">
-                          <a :href="item.href"
-                            :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-100', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                            <span
-                              :class="[item.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
-                                item.initial }}</span>
-                            <span class="truncate">{{ item.name }}</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li class="mt-auto">
-                      <Link :href="route('logout')" method="post" as="button" type="button"
-                        class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600 w-full">
-                      <ArrowLeftOnRectangleIcon class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-                        aria-hidden="true" />
-                      Uitloggen
-                      </Link>
-                    </li>
-                  </ul>
-                </nav>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </Dialog>
-    </TransitionRoot>
-
-    <!-- Static sidebar for desktop -->
-    <div class="hidden lg:fixed lg:z-50 lg:flex lg:w-72 lg:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex grow flex-col gap-y-5 overflow-y-auto px-6 pb-4">
-        <!-- <div class="flex h-16 shrink-0 items-center">
-          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        </div> -->
-        <nav class="flex flex-1 flex-col">
-          <ul role="list" class="flex flex-1 flex-col gap-y-7">
-            <li>
-              <ul role="list" class="-mx-2 space-y-1">
-                <li v-for="item in navigation" :key="item.name">
-                  <Link :href="item.href"
-                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                  <component :is="item.icon"
-                    :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
-                    aria-hidden="true" />
-                  {{ item.name }}
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <ul role="list" class="-mx-2 space-y-1">
-                <li v-for="item in secondNavigation" :key="item.name">
-                  <Link :href="item.href"
-                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                  <component :is="item.icon"
-                    :class="[item.current ? 'text-indigo-600' : 'text-gray-400 group-hover:text-indigo-600', 'h-6 w-6 shrink-0']"
-                    aria-hidden="true" />
-                  {{ item.name }}
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <div class="text-xs font-semibold leading-6 text-gray-400">Categorieën</div>
-              <ul role="list" class="-mx-2 mt-2 space-y-1">
-                <li v-for="item in categoryNavigation" :key="item.name">
-                  <a :href="item.href"
-                    :class="[item.current ? 'bg-gray-50 text-indigo-600' : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-100', 'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold']">
-                    <span
-                      :class="[item.current ? 'text-indigo-600 border-indigo-600' : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600', 'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white']">{{
-                        item.initial }}</span>
-                    <span class="truncate">{{ item.name }}</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <!-- <li class="mt-auto">
-              <Link href="#"
-                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600">
-              <Cog6ToothIcon class="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600" aria-hidden="true" />
-              Settings
-              </Link>
-            </li> -->
-          </ul>
-        </nav>
-      </div>
-    </div>
+    <SideNavigation />
 
     <div class="lg:pl-72">
       <main>
