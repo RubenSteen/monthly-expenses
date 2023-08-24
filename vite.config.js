b2637374e9e09a5b58bcfaaa3d04798b3f419f06
Ruben Steen <rubensteen123@gmail.com>
2023-08-24 13:13:32 +0000

    composer update

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
         hmr: {
             host: 'localhost',
-        },
-        watch: {
-            usePolling: true,
         },
     },
     plugins: [
